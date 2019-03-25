067661d552126ae849a3dc6dcc310fe05a6ad2d1
Colton Lacey and Joe Barnes <joe@>
2019-03-25 16:24:38 -0700

    add photos with interaction

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,102 @@
-var user = prompt("enter a sentence");
- alert(user);
+$(function(){
+  var user = prompt("enter a sentence");
+   alert(user);
 
- var newUser = function(string){
-   return string.charAt(0).toUpperCase();
- };
+   var newUser = function(string){
+     return string.charAt(0).toUpperCase();
+   }
 
- var newUser2 = function(string){
-   return string.charAt(string.length-1).toUpperCase();
- }
+   var newUser2 = function(string){
+     return string.charAt(string.length-1).toUpperCase();
+   }
 
- var divide = function(string) {
-   string = parseInt(user.length) /2;
-   newString = Math.round(string);
-   return newString;
- };
+   var divide = function(string) {
+     string = parseInt(user.length) /2;
+     newString = Math.round(string);
+     return newString;
+   }
 
-// alert(newUser(user) + newUser2(user));
+  // alert(newUser(user) + newUser2(user));
 
-// alert(newUser2(user) + newUser(user));
+  // alert(newUser2(user) + newUser(user));
 
-// alert(user + newUser(user) + newUser2(user));
+  // alert(user + newUser(user) + newUser2(user));
 
-var thisString = function(string) {
-  alert(newUser(string) + newUser2(string)),
-  alert(newUser2(string) + newUser(string)),
-  string2 = string + newUser(string) + newUser2(string)
-  alert(string2),
-  addstring = divide(user)
-  alert(addstring)
-  newstring2 = user.charAt(addstring)
-  string4 = newstring2 + string2
-  alert(string4)
-  finalString = string4.split("").reverse().join("")
-  alert(finalString)
+  var thisString = function(string) {
+    alert(newUser(string) + newUser2(string)),
+    alert(newUser2(string) + newUser(string)),
+    string2 = string + newUser(string) + newUser2(string)
+    alert(string2),
+    addstring = divide(user),
+    alert(addstring),
+    newstring2 = user.charAt(addstring),
+    string4 = newstring2 + string2,
+    alert(string4),
+    finalString = string4.split("").reverse().join(""),
+    alert(finalString)
+  }
+alert(thisString(user))
+
+  $("#img1").click(function() {
+    alert(user)
+  });
+
+  $("#img2").click(function() {
+    alert(finalString)
+  });
 
 
-}
+});
 
 
-alert(thisString(user));
+
+
+
+
+
+
+//
+// var user = prompt("enter a sentence");
+//  alert(user);
+//
+//  var newUser = function(string){
+//    return string.charAt(0).toUpperCase();
+//  };
+//
+//  var newUser2 = function(string){
+//    return string.charAt(string.length-1).toUpperCase();
+//  }
+//
+//  var divide = function(string) {
+//    string = parseInt(user.length) /2;
+//    newString = Math.round(string);
+//    return newString;
+//  };
+//
+// // alert(newUser(user) + newUser2(user));
+//
+// // alert(newUser2(user) + newUser(user));
+//
+// // alert(user + newUser(user) + newUser2(user));
+//
+// var thisString = function(string) {
+//   alert(newUser(string) + newUser2(string)),
+//   alert(newUser2(string) + newUser(string)),
+//   string2 = string + newUser(string) + newUser2(string)
+//   alert(string2),
+//   addstring = divide(user)
+//   alert(addstring)
+//   newstring2 = user.charAt(addstring)
+//   string4 = newstring2 + string2
+//   alert(string4)
+//   finalString = string4.split("").reverse().join("")
+//   alert(finalString)
+//
+//
+// }
+
+
+
 
 
 
